e8fb7528c63988b460119fd1daa241fa36b6bf9f
Rick Pontefract <rjpontefract@gmail.com>
2026-06-07 09:45:00 +1200

    Reinstate speech emulation and make it work better

---
+++ src/speech.h
@@ -0,0 +1,20 @@
+#ifndef BEM_SPEECH
+#define BEM_SPEECH
+
+#include "b-em.h"
+
+extern uint8_t speech_status;
+
+extern void speech_init(void);
+extern void speech_reset(void);
+extern void speech_close(void);
+extern void speech_poll(int c);
+extern int16_t speech_mix(void);
+extern void speech_savestate(FILE *f);
+extern void speech_loadstate(FILE *f);
+extern void speech_set_rs(bool value);
+extern void speech_set_ws(bool value);
+extern void speech_write(uint8_t val);
+extern uint8_t speech_read(void);
+
+#endif
